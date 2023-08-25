e172505b1f2ceee842f1ef67812aa491041cb0af
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-08-24 20:36:14 -0700

    Create instance of the GameManager class and start the game through it.

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
+require_relative "game_manager"
 require_relative "game.rb"
 require_relative "math_question.rb"
 require_relative "player.rb"
+
+# Create a new instance of the GameManager class and start the game
+game_manager = GameManager.new("Player 1", "Player 2")
+game_manager.start_game
